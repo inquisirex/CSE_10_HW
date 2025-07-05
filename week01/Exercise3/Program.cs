d53604c5e1e00aa1c040ebcd2f4a04137fd595a1
inquisirex <rodolfo.velasco.bodart@gmail.com>
2025-07-04 23:41:21 -0600

    Week 1 Excersice 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        Console.WriteLine(number);
+        int guess = 0;
+        while (!(number == guess))
+        {
+            Console.WriteLine("What is your guess?");
+            string magicGuess = Console.ReadLine();
+            guess = int.Parse(magicGuess);
+            if (guess > number)
+            {
+                Console.WriteLine("too High");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("too low");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+        }
+            
+
+
     }
 }
