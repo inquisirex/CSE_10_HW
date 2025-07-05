6bb4b78696ce5f18bbaa568daa16758103a6cc5d
inquisirex <rodolfo.velasco.bodart@gmail.com>
2025-07-05 01:06:36 -0600

    Week 1 Excercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, to end the list type 0:");
+        int lastNumber = -1;
+        int sumTotal = 0;
+        int greatest = 0;
+
+        while (!(lastNumber == 0))
+        {
+            string inputString = Console.ReadLine();
+            int inputNumber = int.Parse(inputString);
+            if (inputNumber == 0)
+            {
+                break;
+            }
+            numbers.Add(inputNumber);
+            lastNumber = inputNumber;
+            sumTotal += inputNumber;
+            if (inputNumber > greatest)
+            {
+                greatest = inputNumber;
+            }
+        }
+        int avgTotal = (sumTotal / numbers.Count);
+        Console.WriteLine($"The sum total is: {sumTotal}");
+        Console.WriteLine($"The average total is: {avgTotal}");
+        Console.WriteLine($"The Greatest number in the list is {greatest}");
     }
 }
